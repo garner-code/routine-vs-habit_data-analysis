0a4feaf7214eac171be5403994a7153cdaf1edb5
lane-sj <sadielane03@gmail.com>
2026-08-04 16:22:34 +1000

    freq histos to vis normalcy of data

--- src/visual_qq_te_reclicks.R
+++ src/visual_qq_te_reclicks.R
@@ -83,3 +83,52 @@ ggsave(
   width = 14,
   height = 8
 )
+
+
+# and some histograms while we are here -----------------------------------
+
+reclicks_te |>
+  filter(sub != 30, dv == "reclicks_mean") |>
+  ggplot(aes(x = reclicks_or_TE)) +
+  geom_histogram(binwidth = 0.1, colour = "#ECCBAEFF", fill = "#ECCBAEFF") +
+  facet_wrap(. ~ block) +
+  plot_style() +
+  theme_classic() +
+  labs(
+    title = "reclicks freq distrib",
+    subtitle = "sub 30 excluded (15 mean reclicks)",
+    x = "mean reclicks"
+  ) +
+  theme(
+    strip.background = element_rect(fill = "white", color = "white", linewidth = 0.5),
+  )
+
+ggsave(
+  "freq_histo_reclicks_faceted_no30.png",
+  path = ("C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/plots"),
+  width = 14,
+  height = 8
+)
+
+reclicks_te |>
+  filter(sub != 30, dv == "TE") |>
+  ggplot(aes(x = reclicks_or_TE)) +
+  geom_histogram(binwidth = 0.01, colour = "#046C9AFF", fill = "#046C9AFF") +
+  facet_wrap(. ~ block) +
+  plot_style() +
+  theme_classic() +
+  labs(
+    title = "TE freq distrib",
+    subtitle = "sub 30 excluded (as in reclicks data)",
+    x = "mean TE score"
+  ) +
+  theme(
+    strip.background = element_rect(fill = "white", color = "white", linewidth = 0.5),
+  )
+
+ggsave(
+  "freq_histo_TE_faceted_no30.png",
+  path = ("C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/plots"),
+  width = 14,
+  height = 8
+)
