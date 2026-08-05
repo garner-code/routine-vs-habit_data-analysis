90c43df90a7778ed1fcac2b676926baa4fb17470
lane-sj <sadielane03@gmail.com>
2026-08-05 15:14:10 +1000

    script written and plots outputted showing distribution of cohs data, cor between cohs and reclicks/te

--- src/visual_cohs.R
+++ src/visual_cohs.R
@@ -1,108 +1,197 @@
 ##################              COHS VISUAL              #######################
 #sadie lane, 2026
 
-#histo
-histogram_cohs(raw_df, sub, auto)
-#ggsave("cohs_auto_histo.png")
+library(tidyverse)
+library(paletteer)
+library(ggrepel)
+setwd("C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/src")
+source("function_dv_histo.R")
+source("function_safe_se.R")
+source("plot_style.R")
+source("function_safe_se.R")
+source("function_cohs_histo.R")
 
-histogram_cohs(raw_df, sub, rout)
-#ggsave("cohs_rout_histo.png")
+setwd("C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res")
 
-
-
-#linearity/scatter
-
-linearity_check_cohs(raw_df, auto, rout) +
-  labs(
-    title = "Small pos corr between habit\n and routine on cohs"
-  )
-
-ggsave(
-  "cohs_routine_by_habit.png",
-  path = ("C:/Users/user/OneDrive - UNSW/2026!/Honours/Data/routine_habit/images_42ps"),
-  width = 14,
-  height = 8,
+#read in data
+averages_democohs <- read_csv(
+  "averages_democohs.csv",
+  na = c("", "NA")
 )
 
 
-#small pos linear rel where as auto increases, rout increases.
+# histograms --------------------------------------------------------------
+
+#auto
+averages_democohs |>
+  ggplot(aes(x = auto)) +
+  geom_histogram(binwidth = 0.1, colour = "#ECCBAEFF", fill = "#ECCBAEFF") +
+  plot_style() +
+  theme_classic()
+
+ggsave(
+  "freq_histo_cohs_auto.png",
+  path = ("C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/plots"),
+  width = 14,
+  height = 8
+)
+
+#routine
+averages_democohs |>
+  ggplot(aes(x = rout)) +
+  geom_histogram(binwidth = 0.1, colour = "#046C9AFF", fill = "#046C9AFF") +
+  plot_style() +
+  theme_classic()
+
+ggsave(
+  "freq_histo_cohs_rout.png",
+  path = ("C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/plots"),
+  width = 14,
+  height = 8
+)
+
+# correlation between automaticity and routine ----------------------------
+
+averages_democohs |>
+  filter(ses == 4, block == "mt", switch == 0) |>
+  ggplot(aes(x = auto, y = rout, label = sub)) +
+  geom_point() +
+  geom_text_repel() +
+  geom_smooth(method = lm, se = F) +
+  plot_style() +
+  theme_classic() +
+  labs(
+    subtitle = "r = 0.378"
+  )
+
+ggsave(
+  "geom_text_rout_v_auto.png",
+  path = ("C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/plots"),
+  width = 14,
+  height = 8
+)
+
+cor(averages_democohs$auto, averages_democohs$rout, method = "pearson")
 
 
-# make df mapped onto averages, check important things --------------------
+# cor cohs v reclicks and te ----------------------------------------------
 
-joint_cohs <- full_join(averages, raw_df, by = 'sub')
-
-joint_cohs_test <- joint_cohs |>
+#make a df for comparison
+#reminder that reclicks are necessarily switch, TE is necessarily stay
+df <- averages_democohs |>
+  select(sub:switch, reclicks_mean, TE, auto, rout) |>
   filter(ses == 4) |>
+  pivot_wider(
+    names_from = switch,
+    values_from = c("reclicks_mean", "TE")
+  ) |>
   mutate(
-    switch = factor(switch, levels = c(0, 1), labels = c("Stay", "Switch")),
-    block = factor (block, levels = c("st", "mt"), labels = c("Singletask", "Multitask"))
-  )
+    reclicks_mean = reclicks_mean_1,
+    TE = TE_0
+  ) |>
+  select(sub, block, reclicks_mean, TE, auto, rout)
 
-#auto v TE
-line_cohs(joint_cohs_test, auto, M_sum_TE)
-ggsave(
-  "auto_v_te_42ps.png",
-  path = ("C:/Users/user/OneDrive - UNSW/2026!/Honours/Data/routine_habit/images_42ps"),
-  width = 14,
-  height = 8,
-)
+df_mt <- df |>
+  filter(block == "mt")
 
-#auto v reclicks
-line_cohs(joint_cohs_test, auto, reclicks_mean)
-ggsave(
-  "auto_v_reclicks_42ps.png",
-  path = ("C:/Users/user/OneDrive - UNSW/2026!/Honours/Data/routine_habit/images_42ps"),
-  width = 14,
-  height = 8,
-)
+df_st <- df |>
+  filter(block == "st")
 
-#with reclicks outlier removed.
-joint_cohs_test |>
-  filter(sub != 30) |>
-  line_cohs(auto, reclicks_mean) +
+#reclicks v auto
+
+df |>
+  ggplot(aes(x = auto, y = reclicks_mean, label = sub)) +
+  geom_point() +
+  geom_text_repel() +
+  geom_smooth(method = lm, se = F) +
+  facet_wrap(. ~ block) +
+  plot_style() +
+  theme_classic() +
   labs(
-    subtitle = "outlier sub 30 removed"
+    subtitle = "r = -0.024\nr mt = -0.080\nr st = 0.05"
   )
 
 ggsave(
-  "auto_v_reclicks_42ps_no_outlier.png",
-  path = ("C:/Users/user/OneDrive - UNSW/2026!/Honours/Data/routine_habit/images_42ps"),
+  "geom_text_reclicks_v_auto.png",
+  path = ("C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/plots"),
   width = 14,
-  height = 8,
+  height = 8
 )
 
-#rout v TE
-line_cohs(joint_cohs_test, rout, M_sum_TE)
-ggsave(
-  "rout_v_te_42ps.png",
-  path = ("C:/Users/user/OneDrive - UNSW/2026!/Honours/Data/routine_habit/images_42ps"),
-  width = 14,
-  height = 8,
-)
+cor(df$reclicks_mean, df$auto, method = "pearson")
+cor(df_mt$reclicks_mean, df_mt$auto, method = "pearson")
+cor(df_st$reclicks_mean, df_st$auto, method = "pearson")
 
-
-
-#rout v reclicks
-line_cohs(joint_cohs_test, rout, reclicks_mean)
-ggsave(
-  "rout_v_reclicks_42ps.png",
-  path = ("C:/Users/user/OneDrive - UNSW/2026!/Honours/Data/routine_habit/images_42ps"),
-  width = 14,
-  height = 8,
-)
-
-#now without outlier sub 30
-joint_cohs_test |>
-  filter(sub != 30) |>
-  line_cohs(rout, reclicks_mean) +
+#reclicks v rout
+df |>
+  ggplot(aes(x = rout, y = reclicks_mean, label = sub)) +
+  geom_point() +
+  geom_text_repel() +
+  geom_smooth(method = lm, se = F) +
+  facet_wrap(. ~ block) +
+  plot_style() +
+  theme_classic() +
   labs(
-    subtitle = "outlier sub 30 removed"
+    subtitle = "r = 0.008\nr mt = -0.090\nr st = 0.14"
   )
 
+cor(df$reclicks_mean, df$rout, method = "pearson")
+cor(df_mt$reclicks_mean, df_mt$rout, method = "pearson")
+cor(df_st$reclicks_mean, df_st$rout, method = "pearson")
+
 ggsave(
-  "rout_v_reclicks_42ps_no_outlier.png",
-  path = ("C:/Users/user/OneDrive - UNSW/2026!/Honours/Data/routine_habit/images_42ps"),
+  "geom_text_reclicks_v_rout.png",
+  path = ("C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/plots"),
   width = 14,
-  height = 8,
+  height = 8
 )
+
+#TE v auto
+df |>
+  ggplot(aes(x = auto, y = TE, label = sub)) +
+  geom_point() +
+  geom_text_repel() +
+  geom_smooth(method = lm, se = F) +
+  facet_wrap(. ~ block) +
+  plot_style() +
+  theme_classic() +
+  labs(
+    subtitle = "r = -0.019\nr mt = 0.011\nr st = -0.051"
+  )
+
+cor(df$TE, df$auto, method = "pearson")
+cor(df_mt$TE, df_mt$auto, method = "pearson")
+cor(df_st$TE, df_st$auto, method = "pearson")
+
+ggsave(
+  "geom_text_TE_v_auto.png",
+  path = ("C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/plots"),
+  width = 14,
+  height = 8
+)
+
+
+#TE v rout
+df |>
+  ggplot(aes(x = rout, y = TE, label = sub)) +
+  geom_point() +
+  geom_text_repel() +
+  geom_smooth(method = lm, se = F) +
+  facet_wrap(. ~ block) +
+  plot_style() +
+  theme_classic() +
+  labs(
+    subtitle = "r = 0.038\nr mt = 0.071\nr st = 0.003"
+  )
+
+cor(df$TE, df$rout, method = "pearson")
+cor(df_mt$TE, df_mt$rout, method = "pearson")
+cor(df_st$TE, df_st$rout, method = "pearson")
+
+ggsave(
+  "geom_text_TE_v_rout.png",
+  path = ("C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/plots"),
+  width = 14,
+  height = 8
+)
+
