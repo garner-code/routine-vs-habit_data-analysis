e5358d6e4175861308901ebc5c9a81ed6a418161
lane-sj <sadielane03@gmail.com>
2026-08-30 15:27:54 +1000

    first attempt rt and tj analysis complete

--- src/analyse/analyse_rt_acc_difs.R
+++ src/analyse/analyse_rt_acc_difs.R
@@ -36,7 +36,28 @@ averages |>
     tjsSD = sd(task_jumps_mean)
   )
 
-#no transform
+#no transform, outliers included
+all_for_t_tests <- averages |>
+  filter(ses == 4 & switch == 0) |>
+  select(sub, block, rt_mean, accuracy_mean, task_jumps_mean) |>
+  pivot_wider(
+    names_from = block,
+    values_from = c(rt_mean, accuracy_mean, task_jumps_mean)
+  )
+
+#log transform, outliers included
+all_for_t_tests_log <- all_for_t_tests |>
+  mutate(
+    rt_mt_log  = log(rt_mean_mt + 0.001),
+    rt_st_log  = log(rt_mean_st + 0.001),
+    tj_mt_log = log(task_jumps_mean_mt + 0.001),
+    tj_st_log = log(task_jumps_mean_st + 0.001),
+  ) |>
+  select(sub, rt_mt_log, rt_st_log, tj_mt_log, tj_st_log)
+
+
+
+#no transform, no outliers
 for_t_tests <- outlierless |>
   filter(ses == 4 & switch == 0) |>
   select(sub, block, rt_mean, accuracy_mean, task_jumps_mean) |>
@@ -45,7 +66,7 @@ for_t_tests <- outlierless |>
     values_from = c(rt_mean, accuracy_mean, task_jumps_mean)
   )
 
-#log transform
+#log transform, no outliers
 for_t_tests_log <- for_t_tests |>
   mutate(
     rt_mt_log  = log(rt_mean_mt + 0.001),
@@ -57,69 +78,8 @@ for_t_tests_log <- for_t_tests |>
 
 # analyse ----------------------------------------------------------------------
 # run t tests and summaries. Output into csvs. #n.b. this is all for switch = 0.
+# task jumps as well as rt
 
-# acc (no transform)
-
-t_acc <- with(for_t_tests, t.test(accuracy_mean_st, accuracy_mean_mt)) |>
-  tidy()
-
-write_csv(
-  t_acc,
-  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/analysis_t_test_acc.csv"
-)
-
-sum_acc <- with(for_t_tests, summary(accuracy_mean_st, accuracy_mean_mt)) |>
-  skim()
-
-sum_acc <- for_t_tests |>
-  skim_without_charts()
-
-write_csv(
-  sum_acc,
-  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/analysis_sum_acc.csv"
-)
-
-#rt (with and without log bc normal distrib)
-
-#without
-t_rt <- with(for_t_tests, t.test(rt_mean_st, rt_mean_mt)) |>
-  tidy()
-
-write_csv(
-  t_rt,
-  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/analysis_t_rt.csv"
-)
-
-sum_rt <- for_t_tests |>
-  skim_without_charts()
-
-write_csv(
-  sum_rt,
-  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/analysis_sum_rt.csv"
-)
-
-#with log
-t_rt_log <- with(for_t_tests, t.test(log(rt_mean_st), log(rt_mean_mt))) |>
-  tidy()
-
-write_csv(
-  t_rt_log,
-  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/analysis_t_rt_log.csv"
-)
-
-sum_log_rt <- for_t_tests_log |>
-  skim_without_charts()
-
-write_csv(
-  sum_log_rt,
-  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/analysis_sum_rt_log.csv"
-)
-
-#keeping above code just in case
-#but accuracy has been deprecated in favour of task jumps
-
-
-# now checking task jumps v reclicks --------------------------------------
 
 #first with outliers taken out
 #which as a reminder is n_nc, tjs, and sens -
@@ -175,17 +135,49 @@ write_csv(
   "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/outless_log_summary.csv"
 )
 
-#second with all participants included
 
-#rt has already been run (see above)
+# now with all participants included -------------------------------------
 
-t_tj <-
+all_t_rt <- with(all_for_t_tests, t.test(rt_mean_st, rt_mean_mt))
+all_t_rt <- tidy(all_t_rt)
+#sig - interesting as outlierless is ns
+write_csv(
+  all_t_rt,
+  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/all_t_rt.csv"
+)
 
-t_tj_log <-
+all_t_rt_log <- with(all_for_t_tests_log, t.test(rt_st_log, rt_mt_log))
+all_t_rt_log <- tidy(all_t_rt_log)
+#sig as well
+write_csv(
+  all_t_rt_log,
+  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/all_t_rt_log.csv"
+)
 
-all_summary <-
+all_t_tj <- with(all_for_t_tests, t.test(task_jumps_mean_st, task_jumps_mean_mt))
+all_t_tj <- tidy(all_t_tj)
+#ns - interesting as outlierless is sig
+write_csv(
+  all_t_tj,
+  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/all_t_tj.csv"
+)
 
-all_log_summary <-
+all_t_tj_log <- with(all_for_t_tests_log, t.test(tj_st_log, tj_mt_log))
+all_t_tj_log <- tidy(all_t_tj_log)
+#log adjusted it i ssig - which seems more parsimonious to me
+write_csv(
+  all_t_tj_log,
+  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/all_t_tj_log.csv"
+)
 
+all_summary <- skim_without_charts(all_for_t_tests)
+write_csv(
+  all_summary,
+  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/all_summary_tj_rt_ttests.csv"
+)
 
-summary(averages)
+all_log_summary <- skim_without_charts(all_for_t_tests_log)
+write_csv(
+  all_log_summary,
+  "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/analysis/all_summary_tj_rt_ttests_log.csv"
+)
