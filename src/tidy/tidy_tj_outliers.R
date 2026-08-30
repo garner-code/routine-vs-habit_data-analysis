fca579192a850e2f0af7bfd53761588dc8595391
lane-sj <sadielane03@gmail.com>
2026-08-30 14:01:00 +1000

    3 outlier criterion removed in prep for t-test

--- src/tidy/tidy_tj_outliers.R
+++ src/tidy/tidy_tj_outliers.R
@@ -1,6 +1,7 @@
-#output csv without task jump outliers
-#where outlier is mean - 2.5sds
-#in this case if more than 30.72 non counted trials
+#sorting outliers for n_nc_trials (pre task jump calc)
+#then task jumps themselves
+#then finally n_back sens criteria
+#kgg and sl 2026
 rm(list=ls())
 library(tidyverse)
 
@@ -18,31 +19,72 @@ averages_democohs <- read_csv(
   na = c("", "NA")
 )
 
-subs2excl <- averages %>%
-  filter(x < 50) %>%
-  unique(sub)
+
+# first exclusion n_nc trials ---------------------------------------------
+
+#output csv without task jump outliers for n_nc_trials
+#where outlier is mean - 2.5sds
+#in this case if more than 30.72 non counted trials
+
 #subs 8, 9, 11 are outliers on tjs
 subs2excl <- c(8,9,11)
 
 averages <- averages %>%
   filter(!sub %in% subs2excl)
+write_csv(averages, "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/averages_no_n_nc_outs.csv")
 
+averages_democohs <- averages_democohs |>
+  filter(!sub %in% subs2excl)
 
+write_csv(averages_democohs, "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/averages_democohs_no_n_nc_outs.csv")
 
-
-averages[43:48, "task_jumps_mean"] <- NA
+#averages[43:48, "task_jumps_mean"] <- NA
 #9
-averages[49:54, "task_jumps_mean"] <- NA
+#averages[49:54, "task_jumps_mean"] <- NA
 #11
-averages[61:66, "task_jumps_mean"] <- NA
+#averages[61:66, "task_jumps_mean"] <- NA
+
+#8
+#averages_democohs[43:48, "task_jumps_mean"] <- NA
+#9
+#averages_democohs[49:54, "task_jumps_mean"] <- NA
+#11
+#averages_democohs[61:66, "task_jumps_mean"] <- NA
+
+
+
+# exclusion 2 - tjs ------------------------------------------------------------
+
+#now get rid of the people who had enough trials to calc a tj score,
+#but whose tj score itself was an outlier.
+#anyone who has even one outlier, across all four permutations
+#i.e across mt or st, stay or switch
+tj_exclude <- c(2, 13, 73, 79, 81, 84)
+
+
+averages <- averages |>
+  filter(!sub %in% tj_exclude)
 
 write_csv(averages, "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/averages_no_tj_outs.csv")
 
-#8
-averages_democohs[43:48, "task_jumps_mean"] <- NA
-#9
-averages_democohs[49:54, "task_jumps_mean"] <- NA
-#11
-averages_democohs[61:66, "task_jumps_mean"] <- NA
+averages_democohs <- averages_democohs |>
+  filter(!sub %in% tj_exclude)
 
 write_csv(averages_democohs, "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/averages_democohs_no_tj_outs.csv")
+
+# exclusion 3 - nback -----------------------------------------------------
+#criteria is < 0.6 across both sens and spec
+#only sens has outliers
+
+sens_exclude <- c(13, 23, 25, 28, 51, 61, 73, 76, 85)
+
+averages <- averages |>
+  filter(!sub %in% sens_exclude)
+
+write_csv(averages, "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/averages_no_tj_no_nback_outs.csv")
+
+averages_democohs <- averages_democohs |>
+  filter(!sub %in% sens_exclude)
+
+write_csv(averages, "C:/Users/Sadie/Repos/routine-vs-habit_data-analysis/res/averages_democohs_no_tj_no_nback_outs.csv")
+
