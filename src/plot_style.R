4e009f6eecea52a110d76481995de96f39799216
lane-sj <sadielane03@gmail.com>
2026-08-31 14:29:06 +1000

    font size increase

--- src/plot_style.R
+++ src/plot_style.R
@@ -2,8 +2,8 @@
 ##############    Sadie Lane 2026 Hons Project - plot style   ##################
 ################################################################################
 plot_style <- function() {
-  title_sz <- 18
-  label_sz <- 14
+  title_sz <- 20
+  label_sz <- 16
   theme_classic() +
     theme(
       plot.title = element_text(size = title_sz),
