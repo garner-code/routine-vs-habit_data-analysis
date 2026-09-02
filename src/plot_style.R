1db9e59e08b7d2be8f0ff7d7cec58ceb89f3ff00
lane-sj <sadielane03@gmail.com>
2026-09-02 14:26:52 +1000

    plot_style fixed

--- src/plot_style.R
+++ src/plot_style.R
@@ -2,16 +2,16 @@
 ##############    Sadie Lane 2026 Hons Project - plot style   ##################
 ################################################################################
 plot_style <- function() {
-  title_sz <- 20
-  label_sz <- 16
+  title_sz <- 16
+  label_sz <- 14
   theme_classic() +
     theme(
       plot.title = element_text(size = title_sz),
       axis.text.x = element_text(size = label_sz),
       axis.text.y = element_text(size = label_sz),
       legend.text = element_text(size = label_sz),
-      axis.title.x = element_text(size = label_sz),
-      axis.title.y = element_text(size = label_sz),
+      axis.title.x = element_text(size = title_sz),
+      axis.title.y = element_text(size = title_sz),
       legend.title = element_text(size = label_sz),
       strip.text.x = element_text(size = label_sz)
     )
